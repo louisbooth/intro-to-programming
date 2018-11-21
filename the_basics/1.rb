8e24a1862e7b5080aaf7068fac5d94e6927cfee2
Louis Booth <louis.booth@hotmail.com>
2018-11-21 23:39:30 +0000

    Exercises 1 and 2

---
+++ the_basics/1.rb
@@ -0,0 +1,5 @@
+first_name = "Louis"
+last_name = "Booth"
+full_name = first_name + " " + last_name
+
+puts full_name
---
+++ the_basics/2.rb
@@ -0,0 +1,9 @@
+thousands = 4936 / 1000
+hundreds = 4936 % 1000 / 100
+tens = 4936 % 100 / 10
+ones = 4936 % 10
+
+puts "thousands: #{thousands}"
+puts "hundreds: #{hundreds}"
+puts "tens: #{tens}"
+puts "ones: #{ones}"
