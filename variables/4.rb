a53d15e9ee626cd808a457fa4ed4ec080940e4cd
Louis Booth <louis.booth@hotmail.com>
2018-11-22 12:09:09 +0000

    Exercise 4

---
+++ variables/4.rb
@@ -0,0 +1,7 @@
+# name.rb continued again
+
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hello, " + first_name.capitalize + " " + last_name.capitalize + "!"
