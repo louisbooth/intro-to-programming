f3a9a77f7d7e745d1bd7d5997237222a1e51d3e9
Louis Booth <louis.booth@hotmail.com>
2018-11-22 00:45:26 +0000

    Exercise 3

---
+++ the_basics/3.rb
@@ -0,0 +1,12 @@
+movies = { 
+    as_years_go_by: 1988,
+    days_of_being_wild: 1990,
+    chungking_express: 1994,
+    ashes_of_time: 1994,
+    fallen_angels:1995 }
+
+puts movies[:as_years_go_by]
+puts movies[:days_of_being_wild]
+puts movies[:chungking_express]
+puts movies[:ashes_of_time]
+puts movies[:fallen_angels]
