5515dd2b86f57940a7d193dc540f6d3c17230d02
Louis Booth <louis.booth@hotmail.com>
2018-11-28 14:44:59 +0000

    Exercise 6 completed

---
+++ methods/6.rb
@@ -0,0 +1,8 @@
+# the method expects two arguments, but is only given one
+
+ArgumentError: wrong number of arguments (1 for 2)
+  from (irb):1:in `calculate_product'
+  from (irb):4
+  from /Users/username/.rvm/rubies/ruby-2.0.0-p353/bin/irb:12:in `<main>'
+  
+  
