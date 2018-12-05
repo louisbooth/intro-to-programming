4870cf06d6d9daed2e924dcac66d97dbcfe8e769
Louis Booth <louis.booth@hotmail.com>
2018-12-05 22:56:57 +0000

    Exercise 6

---
+++ flow_control/6.rb
@@ -0,0 +1,13 @@
+#explain the error in the following code
+
+def equal_to_four(x)
+  if x == 4
+    puts "yup"
+  else
+    puts "nope"
+end
+
+equal_to_four(5)
+
+# there needs to be another end statement
+# methods and if statements each require an end statement
