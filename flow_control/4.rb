18fd149ad6b55e8dec3f9474bb8a7f80e4a7590a
Louis Booth <louis.booth@hotmail.com>
2018-12-05 14:13:48 +0000

    Exercise 4

---
+++ flow_control/4.rb
@@ -0,0 +1,28 @@
+'4' == 4 ? puts("TRUE") : puts("FALSE")
+# my answer = false. CORRECT!
+
+x = 2
+if ((x * 3) / 2) == (4 + 4 - x - 3)
+    puts "Did you get it right?"
+else
+    puts "Did you?"
+end
+# my answer = "Did you get it right?". CORRECT!
+
+y = 9
+x = 10
+if (x + 1) <= (y)
+    puts "Alright."
+elsif (x + 1) >= (y)
+    puts "Alright now!"
+elsif (y + 1) == x
+    puts "ALRIGHT NOW!"
+else
+    puts "Alrighty!"
+end
+# my answer = both "Alright now!" and "ALRIGHT NOW!"
+# INCORRECT! only "Alright now!" ...?
+
+
+
+
