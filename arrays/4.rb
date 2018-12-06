825f4520f2b87d51bc310ae76ec8ffcaefd26835
Louis Booth <louis.booth@hotmail.com>
2018-12-06 17:29:41 +0000

    Exercise 4

---
+++ arrays/4.rb
@@ -0,0 +1,12 @@
+# what does each method return?
+
+arr = [15, 7, 18, 5, 12, 8, 5, 1]
+
+1. arr.index(5)
+# returns 3
+
+2. arr.index[5]
+# returns an error, should be () instead of []
+
+3. arr[5]
+# returns 8
