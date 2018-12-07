7bc03e608a25a24f5660293b4220e6e303ce3f91
Louis Booth <louis.booth@hotmail.com>
2018-12-07 14:04:11 +0000

    Exercise 6

---
+++ arrays/6.rb
@@ -0,0 +1,12 @@
+# names = ['bob', 'joe', 'susan', 'margaret']
+# names['margaret'] = 'jody'
+
+# when using array assignment method, use the index
+# of that element rather than the string itself
+
+# correct syntax below
+
+names = ['bob', 'joe', 'susan', 'margaret']
+names[3] = 'jody'
+
+p names
