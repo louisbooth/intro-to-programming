391449f380013d7a08171aaa072ee2b6b35c87a1
Louis Booth <louis.booth@hotmail.com>
2018-12-07 03:33:37 +0000

    Exercise 5

---
+++ arrays/5.rb
@@ -0,0 +1,12 @@
+# What is the value of a, b, and c in the following program?
+
+string = "Welcome to America!"
+
+a = string[6]
+b = string[11]
+c = string[19]
+
+# a is "e"
+# b is "A"
+# c is nil
+
